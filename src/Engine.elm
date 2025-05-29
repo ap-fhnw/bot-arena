bc1fb1c8708259fca4e5cdff89fb3e697720f383
Mike Gilgen <mgilgen@protonmail.ch>
2025-05-29 11:48:33 +0200

    feat: boundary check

--- src/Engine.elm
+++ src/Engine.elm
@@ -1,6 +1,7 @@
 module Engine exposing (tick)
 
-import Model exposing (World, Bot, Instr(..))
+import Model exposing (World, Bot, Instr(..), Obj(..))
+import Html.Attributes exposing (coords)
 
 scanEnvironment : World -> Bot -> a
 scanEnvironment w b =
@@ -11,12 +12,43 @@ scanEnvironment w b =
 runBot : World -> Bot -> Bot
 runBot w b = case (List.drop b.pc b.program) of
     -- Move as long as there is no wall or object in the way
-    (Move n :: _) -> { b | pc = b.pc + 1, pos = case b.dirDeg of
-         0 -> (Tuple.first b.pos - n, Tuple.second b.pos) -- Move up
-         90 -> (Tuple.first b.pos, Tuple.second b.pos + n) -- Move right
-         180 -> (Tuple.first b.pos + n, Tuple.second b.pos) -- Move down
-         270 -> (Tuple.first b.pos, Tuple.second b.pos - n) -- Move left
-         _ -> (Tuple.first b.pos, Tuple.second b.pos) } 
+    (Move n :: _) -> 
+        let  
+            -- Get step direction based on bot orientation
+            (dx, dy) = case b.dirDeg of
+                0   -> (-1, 0)  -- Move up
+                90  -> (0, 1)   -- Move right
+                180 -> (1, 0)   -- Move down
+                270 -> (0, -1)  -- Move left
+                _   -> (0, 0)   -- Don't move 
+            
+            -- Check if position is valid (within bounds, no wall, no bot)
+            isValidPos (x, y) = 
+                x >= 0 && x < Tuple.second(w.arena.size) && y >= 0 && y < Tuple.first(w.arena.size) &&
+                not (List.any (\obj ->
+                    case obj of
+                        Wall coord -> coord == (x, y)
+                        _ -> False) w.arena.objects) &&
+
+                not (List.any (\bot -> bot.id /= b.id && bot.alive && bot.pos == (x, y)) w.bots)
+
+            -- Try to move step by step
+            moveStepByStep steps (x, y) = 
+                if steps <= 0 then
+                    (x, y) -- reached target
+                else
+                    let
+                        nextPos = (x + dx, y + dy)
+                    in
+                    if isValidPos nextPos then
+                        moveStepByStep (steps - 1) nextPos
+                    else
+                        (x, y)
+            -- calculate the new position
+            newPos = moveStepByStep n b.pos
+        in 
+        { b | pc = b.pc + 1, pos = newPos }
+
     -- Turn 0, 90, 180, 270 degrees (up, right, down, left)
     (Turn n :: _) -> { b | pc = b.pc + 1, dirDeg = modBy 360 (b.dirDeg + n) }
     -- Scan environment, view angle is 90 degrees -> 45 degrees left and right
