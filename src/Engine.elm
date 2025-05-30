b08b26583f7d610748f4b955d7210ac4a0586aa0
Mike Gilgen <mgilgen@protonmail.ch>
2025-05-30 17:09:36 +0200

    simplify and fix while for direct execution

--- src/Engine.elm
+++ src/Engine.elm
@@ -59,6 +59,45 @@ getBotDirAndPos bot =
     in
     (dirVec, bot.pos)
 
+-- Helper function to calculate movement with collision detection
+moveBot : World -> BotEntity -> Int -> Model.Coord
+moveBot w b steps =
+    if not b.alive then
+        -- If the bot is not alive, it cannot move
+        b.pos
+    else
+    let
+        -- Try to move step by step
+        moveStepByStep remainingSteps (x, y) = 
+            let
+                -- Get step direction based on bot orientation
+                ((dx, dy), (_, _)) = getBotDirAndPos b
+
+                -- Check if position is valid (within bounds, no wall, no bot)
+                isValidPos (x2, y2) = 
+                    x2 >= 0 && x2 <= Tuple.second(w.arena.size) && 
+                    y2 >= 0 && y2 <= Tuple.first(w.arena.size) &&
+                    not (List.any (\obj ->
+                        case obj of
+                            Wall coord -> coord == (x2, y2)
+                            _ -> False) w.arena.objects
+                        ) &&
+                    not (List.any (\bot -> 
+                        bot.id /= b.id && liveBotAt bot (x2, y2)) w.bots)
+            in
+            if remainingSteps <= 0 then
+                (x, y) -- reached target
+            else
+                let
+                    nextPos = (x + dx, y + dy)
+                in
+                if isValidPos nextPos then
+                    moveStepByStep (remainingSteps - 1) nextPos
+                else
+                    (x, y)
+    in
+    moveStepByStep steps b.pos
+
 evalCond : World -> BotEntity -> Cond -> Bool
 evalCond w b cond =
     case cond of
@@ -87,46 +126,19 @@ evalCond w b cond =
         Not c ->
             not (evalCond w b c)
 
+-- Helper function to insert an instruction at the next program counter position
+-- Used in If-Then-Else Conditional
+insertInstrAtNextPc : Instr -> BotEntity -> BotEntity
+insertInstrAtNextPc instr bot =
+    let
+        newProgram = List.take (bot.pc + 1) bot.program ++ (instr :: List.drop (bot.pc + 1) bot.program)
+    in
+    { bot | pc = bot.pc + 1, program = newProgram }
+
 runBot : World -> BotEntity -> BotEntity
 runBot w b = case (List.drop b.pc b.program ) of
     -- Move as long as there is no wall or object in the way
-    (Move n :: _) -> 
-        if not b.alive then
-            -- If the bot is not alive, skip the move
-            { b | pc = b.pc + 1 }
-        else
-        let  
-            -- Try to move step by step
-            moveStepByStep steps (x, y) = 
-                let
-                    -- Get step direction based on bot orientation
-                    ((dx, dy), (_, _))  = getBotDirAndPos b
-
-                    -- Check if position is valid (within bounds, no wall, no bot)
-                    isValidPos (x2, y2) = 
-                        x2 >= 0 && x2 <= Tuple.second(w.arena.size) && y2 >= 0 && y2 <= Tuple.first(w.arena.size) &&
-                        not (List.any (\obj ->
-                            case obj of
-                                Wall coord -> coord == (x2, y2)
-                                _ -> False) w.arena.objects
-                            ) &&
-                        not (List.any (\bot -> bot.id /= b.id && liveBotAt bot (x2, y2)) w.bots)
-                in
-                if steps <= 0 then
-                    (x, y) -- reached target
-                else
-                    let
-                        nextPos = (x + dx, y + dy)
-                    in
-                    if isValidPos nextPos then
-                        moveStepByStep (steps - 1) nextPos
-                    else
-                        (x, y)
-            -- calculate the new position
-            newPos = moveStepByStep n b.pos
-        in 
-        { b | pc = b.pc + 1, pos = newPos }
-
+    (Move n :: _) -> { b | pc = b.pc + 1, pos = moveBot w b n}
     -- Turn 0, 90, 180, 270 degrees (up, right, down, left)
     (Turn n :: _) -> { b | pc = b.pc + 1, dirDeg = modBy 360 (b.dirDeg + n) }
     -- Scan environment, view angle is 90 degrees -> 45 degrees left and right
@@ -136,43 +148,31 @@ runBot w b = case (List.drop b.pc b.program ) of
     -- If-then-else instruction
     (IfThenElse cond ifTrue ifFalse :: _) ->
         if evalCond w b cond then
-            -- If condition is true, execute the true branch
-            let
-                newProgram = List.take (b.pc + 1) b.program ++ (ifTrue :: List.drop (b.pc + 1) b.program)
-            in
-            if b.pc + 1 < List.length b.program then
-                -- Insert the true branch at the current position
-                { b | pc = b.pc + 1, program = newProgram }
-            else
-                -- If we are at the end of the program, just append the true branch
-            { b | pc = b.pc + 1, program = newProgram }
+            insertInstrAtNextPc ifTrue b
         else
             -- If condition is false, execute the false branch
-            let
-                newProgram = List.take (b.pc + 1) b.program ++ (ifFalse :: List.drop (b.pc + 1) b.program)
-            in
-            if b.pc + 1 < List.length b.program then
-                -- Insert the false branch at the current position
-                { b | pc = b.pc + 1, program = newProgram }
-            else
-                -- If we are at the end of the program, just append the false branch
-            { b | pc = b.pc + 1, program = newProgram }
+            insertInstrAtNextPc ifFalse b
 
     -- While loop
     (While cond body :: _) -> 
         if evalCond w b cond then
             -- If condition is true, execute the body
             let
+                botAfterBody = case body of
+                    Move n -> { b | pos = moveBot w b n }
+                    Turn n -> { b | dirDeg = modBy 360 (b.dirDeg + n) }
+                    Scan -> { b | viewEnv = scanEnvironment w b }
+                    Fire _ _ -> { b | pc = b.pc + 1 } -- Fire does not change the bot state
+                    _ -> { b | pc = b.pc + 1 } -- No operation, just move to next instruction
+
                 -- Create a new Instruction list with the body repeated
-                newProgram = 
-                    List.take b.pc b.program ++ [body, While cond body] ++ List.drop (b.pc + 1) b.program
+                newProgram = List.take (b.pc) b.program ++ (While cond body :: List.drop (b.pc + 1) b.program)
             in
                 -- Insert the repeated instructions at the current position
-                { b | program = newProgram }
+                { botAfterBody | program = newProgram }
             -- If condition is false, just skip to the next instruction
             else
                 { b | pc = b.pc + 1 }
-
 
     -- Repeat instruction
     (Repeat n body :: _) ->
