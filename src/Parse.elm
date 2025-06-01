de7930f55147cc2bbce038151b44f541c51653ba
ap-fhnw <andro.pavlovic@students.fhnw.ch>
2025-06-01 19:01:14 +0200

    Update Fire Instruction use bot range

--- src/Parse.elm
+++ src/Parse.elm
@@ -49,13 +49,16 @@ isSpace c = c == ' ' || c == '\n'
 spaces : Parser ()
 spaces = takeWhile isSpace |> map (\_ -> ())
 
-
 intToken : Parser Int
-intToken = takeWhile Char.isDigit |> andThen
-    (\s -> case String.toInt s of
-        Just n -> succeed n
-        Nothing -> fail "invalid integer literal"
-    )
+intToken =
+    let
+        digits = takeWhile Char.isDigit |> andThen 
+            (\s -> case String.toInt s of
+                Just n  -> succeed n
+                Nothing -> fail "invalid integer literal"
+            )
+    in
+        ignoreRight digits spaces
 
 token : String -> Parser String
 token str = ignoreLeft spaces (ignoreRight (string str) spaces)
